c06cd3b41c31d890b5612392054e7f29de6a2e2c
unknown <bigmbushari@gmail.com>
2024-09-16 08:58:13 +0400

    fix: update the host to the domain

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   // baseURL: import.meta.env.VITE_BASE_URL,
   // baseURL: "http://10.255.254.112:3000",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://qubit.ae",
   headers: {
     Accept: "*/*",
     Authorization: "Bearer " + window.localStorage.getItem("qbt") || "",
